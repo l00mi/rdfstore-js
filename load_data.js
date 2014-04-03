dad4bf79b2ab804935dc80c4b47199f78acb4b82
Michael Luggen <github.michael@oiu.ch>
2014-04-03 16:16:18 +0200

    dists updated with latests changes

--- load_data.js
+++ load_data.js
@@ -2,7 +2,7 @@ var Store = require('./src/js-store/src/store.js');
 
 Store.Store.create(function(store) {
 
-  store.load('text/n3', 'file://Users/antonio/Development/Projects/js/rdfstore-js/data/sp2b_10k.n3', 'test_file', function(success, result) {
+  store.load('text/n3', 'data/sp2b_10k.n3', 'test_file', function(success, result) {
       console.log('Finished!');
       console.log(result);
 
